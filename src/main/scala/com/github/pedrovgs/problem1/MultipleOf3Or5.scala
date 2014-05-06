bb395bd991ac47223aa411e7b1dcfd589d8bdac5
pedrovgs <pedrovicente.gomez@gmail.com>
2014-05-07 01:53:18 +0200

    First implementation finished.

--- src/main/scala/com/github/pedrovgs/problem1/MultipleOf3Or5.scala
+++ src/main/scala/com/github/pedrovgs/problem1/MultipleOf3Or5.scala
@@ -11,5 +11,19 @@ package com.github.pedrovgs.problem1
  */
 object MultipleOf3Or5 {
 
- 
+  def main(args: Array[String]) {
+    val start = System.currentTimeMillis();
+    println("If you sum multiples of 3 or 5 below 10 you get: " + sumMultiplesOf3Or5Below(1000))
+    println("Time spent to calculate with 1000 elements: " + (System.currentTimeMillis() - start) + " ms")
+  }
+
+  def sumMultiplesOf3Or5Below(bound: Int): Int = {
+    val multiples = (1 to bound - 1).filter(elem => elem % 3 == 0 || elem % 5 == 0)
+
+    def sumElements(elements: List[Int]): Int = {
+      if (elements.isEmpty) 0
+      else elements.head + sumElements(elements.tail)
+    }
+    sumElements(multiples.toList)
+  }
 }
