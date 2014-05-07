db885c11ee40127e1e5c09fecaebbb092d1690ba
pedrovgs <pedrovicente.gomez@gmail.com>
2014-05-08 01:22:22 +0200

    Add first tail recursive solution

--- src/test/scala/com/github/pedrovgs/problem2/EvenFibonacciSumSuite.scala
+++ src/test/scala/com/github/pedrovgs/problem2/EvenFibonacciSumSuite.scala
@@ -13,15 +13,15 @@ class EvenFibonacciSumSuite extends FunSuite {
 
 
   test("Fibonacci even numbers sums with bound at 4 is 2") {
-    assert(2 === EvenFibonacciSum.sumFibonacciEvenNumbers(4))
+    assert(2 === EvenFibonacciSum.sumFibonacciEvenNumbersRecursive(4))
   }
 
   test("Fibonacci even numbers sums with bound at 6 is 10") {
-    assert(2 === EvenFibonacciSum.sumFibonacciEvenNumbers(6))
+    assert(2 === EvenFibonacciSum.sumFibonacciEvenNumbersRecursive(6))
   }
 
   test("Fibonacci even numbers sums with bound at 9 is 44") {
-    assert(10 === EvenFibonacciSum.sumFibonacciEvenNumbers(9))
+    assert(10 === EvenFibonacciSum.sumFibonacciEvenNumbersRecursive(9))
   }
 
 }
