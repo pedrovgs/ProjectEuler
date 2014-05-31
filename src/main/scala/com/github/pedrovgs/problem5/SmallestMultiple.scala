79fd97d97eb310b4d85ed145aef3e599f684ffae
pedrovgs <pedrovicente.gomez@gmail.com>
2014-05-31 17:28:59 +0200

    Add implementation based on functional combinators and test to cover this implementation

--- src/main/scala/com/github/pedrovgs/problem5/SmallestMultiple.scala
+++ src/main/scala/com/github/pedrovgs/problem5/SmallestMultiple.scala
@@ -14,8 +14,11 @@ object SmallestMultiple {
 
   def main(args: Array[String]) = {
 
-    Time.measure(println("Smallest number multiple of 1 to 20 is: " +
+    Time.measure(println("Smallest number multiple of 1 to 20 with parallel implementation is: " +
       getSmallestMultipleOfFirst20()))
+
+    Time.measure(println("Smallest number multiple of 1 to 20 with parallel implementation is: " +
+      getSmallestMultipleOfFirst20UsingFunctionalCombinators()))
   }
 
   /**
@@ -30,14 +33,25 @@ object SmallestMultiple {
     @tailrec
     def smallestMultipleInner(number: Int): Int = {
       if (isMultipleOf20First(number)) number
-      else smallestMultipleInner(number - 1)
+      else smallestMultipleInner(number + 20)
     }
-
-    def isMultipleOf20First(number: Int): Boolean = {
-      val results = (1 to 20).view.filter(number % _ == 0)
-      results.size == 20
-    }
-
-    smallestMultipleInner(Int.MaxValue)
+    smallestMultipleInner(20)
   }
+
+  /**
+   * Calculate the smallest number multiple of 1 to 20 using functional combinators and view.
+   *
+   * This algorithm is faster because view lets you generate the range and evaluate the result
+   * at the same time.
+   *
+   * @return
+   */
+  def getSmallestMultipleOfFirst20UsingFunctionalCombinators(): Int = {
+    (20 to Int.MaxValue by 20).view.find(isMultipleOf20First).get
+  }
+
+  def isMultipleOf20First(number: Int): Boolean = {
+    (1 to 20).view.filter(number % _ == 0).size == 20
+  }
+
 }
--- src/test/scala/com/github/pedrovgs/problem5/SmallestMultipleSuite.scala
+++ src/test/scala/com/github/pedrovgs/problem5/SmallestMultipleSuite.scala
@@ -3,12 +3,17 @@ package com.github.pedrovgs.problem5
 import org.scalatest.FunSuite
 
 /**
+ *
  * @author Pedro Vicente Gómez Sánchez
  */
 class SmallestMultipleSuite extends FunSuite {
 
   test("Smallest multiple of 1 to 20 is 232792560") {
     assert(232792560 === SmallestMultiple.getSmallestMultipleOfFirst20())
+  }
+
+  test("Smallest multiple of 1 to 20 is 232792560 fucntional combinators implementation") {
+    assert(232792560 === SmallestMultiple.getSmallestMultipleOfFirst20UsingFunctionalCombinators())
   }
 
 }
