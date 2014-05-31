fb9098fa9bcd06519af85217b2683ea435b18ae6
pedrovgs <pedrovicente.gomez@gmail.com>
2014-06-01 01:39:37 +0200

    Add implementation to problem 10

---
+++ src/main/scala/com/github/pedrovgs/problem10/SummationOfPrimes.scala
@@ -0,0 +1,39 @@
+package com.github.pedrovgs.problem10
+
+import com.github.pedrovgs.time.Time
+
+/**
+ *
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ *
+ * @author Pedro Vicente Gómez Sánchez
+ */
+object SummationOfPrimes {
+
+  def main(args: Array[String]) {
+    Time.measure(println("The fum of all the primes below two million is: " + getSummationOfPrimesBelow(2000000)))
+  }
+
+  /**
+   * Implementation based on generate a range from 2 to the bound, filter the prime values and sum every prime number
+   * below the bound. This solution is not too much slow because we have used a isPrime function faster than the easier
+   * version where you search a multiple of the value that is not the value.
+   *
+   * I've improved part of the algorithm generating a initial range of numbers to be evaluated without even numbers
+   * except 2 -> 2 + (3L to bound by 2). With this part of the algorithm we avoid check if even numbers are prime or not.
+   *
+   * @param bound
+   * @return
+   */
+  def getSummationOfPrimesBelow(bound: Long): Long = {
+
+    def isPrime(value: Long): Boolean = {
+      !(2L to math.sqrt(value).toInt).exists(value % _ == 0)
+    }
+
+    2 + (3L to bound by 2).filter(isPrime).sum
+  }
+
+}
