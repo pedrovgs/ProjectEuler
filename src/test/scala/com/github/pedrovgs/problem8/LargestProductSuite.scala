60bf2d80aacce84ec1d82fc8c38f416991940968
pedrovgs <pedrovicente.gomez@gmail.com>
2014-06-01 00:45:37 +0200

    Using BigInt to support solution size instead of Int

--- src/test/scala/com/github/pedrovgs/problem8/LargestProductSuite.scala
+++ src/test/scala/com/github/pedrovgs/problem8/LargestProductSuite.scala
@@ -8,8 +8,12 @@ import org.scalatest.FunSuite
  */
 class LargestProductSuite extends FunSuite {
 
-  test("Largest product of 4 digits adjacent numbers in the exercise attached number is 40824") {
-    assert(40824 === LargestProduct.getLargestProduct())
+  test("Largest product of 4 digits adjacent numbers in the exercise attached number is 5832") {
+    assert(5832 === LargestProduct.getLargestProduct(4))
+  }
+
+  test("Largest product of 13 digits adjacent numbers in the exercise attached number is 23514624000") {
+    assert(BigInt("23514624000") === LargestProduct.getLargestProduct(13))
   }
 
 }
