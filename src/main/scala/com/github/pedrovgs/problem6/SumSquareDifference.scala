6adb1193727dc6a371c2d141637e774b57ea42d2
pedrovgs <pedrovicente.gomez@gmail.com>
2014-05-31 17:58:31 +0200

    Add implementation to problem 6 based on functional combinators

--- src/main/scala/com/github/pedrovgs/problem6/SumSquareDifference.scala
+++ src/main/scala/com/github/pedrovgs/problem6/SumSquareDifference.scala
@@ -1,14 +1,34 @@
 package com.github.pedrovgs.problem6
 
+import com.github.pedrovgs.time.Time
+
 /**
+ * The sum of the squares of the first ten natural numbers is, 12 + 22 + ... + 102 = 385
+ * The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)2 = 552 = 3025
+ *
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ *
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
  *
  * @author Pedro Vicente Gómez Sánchez
  */
 object SumSquareDifference {
 
-  def getSumSquareDifference(range: Range): Int = {
+  def main(args: Array[String]) = {
+    Time.measure(println("The difference between the sum of the squares of the first one hundred natural numbers and the " +
+      "square of the sum is: " + getSumSquareDifference((1 to 100).toList)))
+  }
 
-    0
+  /**
+   *
+   * Calculates the difference between the sum of the squares of a range and the square of the sum of that range.
+   *
+   * @param range to evaluate
+   * @return
+   */
+  def getSumSquareDifference(range: List[Int]): Int = {
+    val sum: Int = range.sum
+    (sum * sum) - range.map(n => n * n).sum
   }
 
 }
--- src/test/scala/com/github/pedrovgs/problem6/SumSquareDifferenceSuite.scala
+++ src/test/scala/com/github/pedrovgs/problem6/SumSquareDifferenceSuite.scala
@@ -8,7 +8,12 @@ import org.scalatest.FunSuite
 class SumSquareDifferenceSuite extends FunSuite {
 
   test("The difference between the sum of the squares of the first ten natural numbers and the square of the sum is 2640") {
-    assert(2640 === SumSquareDifference.getSumSquareDifference(1 to 10))
+    assert(2640 === SumSquareDifference.getSumSquareDifference((1 to 10).toList))
+  }
+
+
+  test("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is 25164150") {
+    assert(25164150 === SumSquareDifference.getSumSquareDifference((1 to 100).toList))
   }
 
 }
